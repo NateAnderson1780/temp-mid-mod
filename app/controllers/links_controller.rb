6ab5f84187b77d361da2e95725d4e7d6da995208
Nate Anderson <curtislowe10@gmail.com>
2016-12-19 15:23:37 -0700

    make new and create methods in links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,22 @@
 class LinksController < ApplicationController
   def index
-    
+    @link = Link.new
+    @links = Link.all
+  end
+  
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      current_user.links << @link
+      redirect :back
+    else
+      redirect :back
+    end
+  end
+  
+  private
+  
+  def link_params
+    params.require(:link).permit(:url, :title)
   end
 end
