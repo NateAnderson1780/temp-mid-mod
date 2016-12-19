a25d7e76eb89741f5694b5a57b3222370e1ddb42
Nate Anderson <curtislowe10@gmail.com>
2016-12-19 16:19:11 -0700

    change attribute name for url on links to url_link

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219215641) do
+ActiveRecord::Schema.define(version: 20161219222730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
-    t.string   "url"
+    t.string   "url_link"
     t.boolean  "read",       default: false
     t.datetime "created_at"
     t.datetime "updated_at"
