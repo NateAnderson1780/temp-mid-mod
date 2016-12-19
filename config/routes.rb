40d1828abe54094c0bd3514887cb185ae203c935
Nate Anderson <curtislowe10@gmail.com>
2016-12-19 15:25:39 -0700

    add authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  root to: "links#index"
+  root to: 'home#index'
+  
+  get 'login_or_sign_up', to: 'welcome#index', as: 'welcome'
+  
+  get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 
   resources :links, only: [:index]
+  resources :users, only: [:create]
 
   namespace :api do
     namespace :v1 do
