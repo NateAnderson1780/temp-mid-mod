3cc8ba51a456fc3572b6ff32e24051c229d48eca
Nate Anderson <curtislowe10@gmail.com>
2017-01-09 10:30:54 -0700

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: {message: "Email already in use"}
+  validates_presence_of :password
+  validates_confirmation_of :password
 end
