06fb1e7cad1769610d42249508d5d1287c1c2aef
Nate Anderson <curtislowe10@gmail.com>
2017-01-09 13:04:09 -0700

    add associations between link and user

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,4 +6,6 @@ class Link < ApplicationRecord
       .group("links.url")
       .order('count("reads".id) DESC').limit(10)
   }
+  validates :url, url: true
+  belongs_to :user 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :links
   validates :email, presence: true, uniqueness: {message: "Email already in use"}
   validates_presence_of :password
   validates_confirmation_of :password
